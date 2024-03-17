7f906334996a6f6e510f2606d9c934b8eb2055e9
sunita shivapuje <sunitateerthappa@gmail.com>
2024-03-17 21:20:48 +0530

    updeted-file

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ProductList from "./agritrade/ProductList";
 import ProductDetail from "./agritrade/ProductDetail";
+import SellerForm from "./agritrade/SellerForm";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<ProductList />} />
         <Route path="/product/:id" element={<ProductDetail />} />
+        <Route path="/sell" element={<SellerForm />} />
       </Routes>
     </Router>
   );
